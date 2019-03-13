a56b7981971b9c41f28e674a7c13294e77751694
bvtujo <austinrossely@gmail.com>
2019-03-13 11:26:02 -0700

    ??? confusion in adding argparse branch

--- beecli.py
+++ beecli.py
@@ -3,6 +3,19 @@ import sys
 import time
 import datetime
 import settings
+import re
+
+NOW = datetime.datetime.now().timetuple()
+CURRENTYEAR = NOW.tm_year
+CURRENTMONTH = NOW.tm_mon
+CURRENTDAY = NOW.tm_mday
+
+
+monsterRegex = r'^\s*((?:(?:0?[1-9]|1\d|2\d|3[01])|(?:0?[1-9]|1[012])\s+(?:0?[1-9]|1\d|2\d|3[01])|(?:19[789]\d|20\d\d)\s+(?:0?[1-9]|1[012])\s+(?:0?[1-9]|1\d|2\d|3[01]))|\^+)\s+([\+\-]?(?:\d+\.?\d*|\.\d+)|\d+st(?:\d+\.?\d*|\.\d+)?|(?:\d*\:[0-5]\d(?:\:[0-5]\d)?|0?0?\:\d\d))(?:(?:\s+-(.*))|(?:\s+[\"\'\u201c\u201d](.*)[\"\'\u201c\u201d])?)\s*$'
+m = re.match(monsterRegex, "2019 3 9 75 -comment")
+n = re.match(monsterRegex, "2019 3 8 75 \"fdsj\"")
+print(m.groups())
+print(n.groups())
 
 def timeTupleFromTimestamp(timestamp):
 	return datetime.datetime.fromtimestamp(timestamp).timetuple()
@@ -11,7 +24,6 @@ def timestampFromTimeTuple(timeTuple):
 	return time.mktime(timeTuple)
 
 def timestampFromYMD(year, month, day, h=0, m=0):
-	if month<1:
 	dt = datetime.datetime(year, month, day, h, m)
 	return time.mktime(dt.timetuple())
 
@@ -19,48 +31,79 @@ def timestampNow():
 	return time.time()
 
 def timeTupleNow():
-	return timeTupleFromTimestamp(time.time())
+	return timeTupleFromTimestamp(timestampNow())
+ 
 
-def addData()
+def parseArgs(args, now=NOW):
+	l = len(args)
+	G = args[1]; #arg 1 is always goalname
+
+	reDig = r'^\d+$' #match any number of digits
+	reAl = r':alpha:+'
+	argMatches = [re.match(reDig, i)  for i in args[1:]]
+	nums = [(i,args[i+1]) for i in range(len(argMatches)) if argMatches[i] != None]
+	comment = True if re.match(reAl, args[-1]) else False
+	print(args)
+	print(nums)
+	print("comment", comment)
+
+
+	HMSFMT = "%H:%M:%S"
+	strtime = time.strftime(HMSFMT,now)
+	if l < 3:
+		print("Too few arguments")
+	elif l == 3:
+		#assume goal & value, date=today
+		DT = now
+		DV = args[2]
+		DC = f"via beecli at {strtime}"
+	elif l == 4:
+		#assume goal, day of month, value
+		DV = args[3]
+	else:
+		return
+
+	return G, DT, DV, DC
+
 
 if __name__=="__main__":
+
 	print(sys.argv)
-	BEEMINDER_USER = settings.BEEMINDER_USER
-	BEEMINDER_API_TOKEN = settings.BEEMINDER_AUTH_TOKEN
+	print(parseArgs(sys.argv))
+	input("press any key")
+	BEEMINDER_USERNAME = settings.BEEMINDER_USERNAME
+	BEEMINDER_AUTH_TOKEN = settings.BEEMINDER_AUTH_TOKEN
 
 	BEEMINDER_GOAL = ''
 	DATAPOINT_TIME = 0
 	DATAPOINT_VALUE = 0
 	DATAPOINT_COMMENT = ''
-	NOW = time.time()
-	NOW_T = timeTupleFromTimestamp(NOW)
-	NOW_D = NOW_T[2]
 	HMSFMT = "%H:%M:%S"
 
 	logfile = "../misc/beecli.log"
 
-	bmndr = beem.Beeminder(BEEMINDER_API_TOKEN)
+	bmndr = beem.Beeminder(BEEMINDER_AUTH_TOKEN)
 
 	args = sys.argv
-	print(args)
-
 	BEEMINDER_GOAL = args[1]
+	print("Beeminder goal: " + BEEMINDER_GOAL)
 
 	if len(args) < 3:
 		print("Too few arguments")
-		time.sleep(2)
 		quit()
 	elif len(args) == 3:
 		#assume goal, value, fill in time=now
-		DATAPOINT_TIME = NOW
+		DATAPOINT_TIME = timestampFromTimeTuple(NOW)
 		DATAPOINT_VALUE = args[2]
 		strtime = time.strftime(HMSFMT,time.localtime())
 		DATAPOINT_COMMENT = f"added at {strtime} by beecli"
+		print(DATAPOINT_TIME)
+		print(DATAPOINT_VALUE)
 	elif len(args) == 4:
-		if int(args[2]) > NOW_D:
-			DATAPOINT_TIME = timestampFromYMD(NOW_T[0], NOW_T[1]-1, int(args[2]))
+		if int(args[2]) > CURRENTDAY:
+			DATAPOINT_TIME = timestampFromYMD(NOW[0], NOW[1]-1, int(args[2]), h=12)
 		else:
-			DATAPOINT_TIME = timestampFromYMD(NOW_T[0], NOW_T[1], int(args[2]))
+			DATAPOINT_TIME = timestampFromYMD(NOW[0], NOW[1], int(args[2]), h=12)
 		DATAPOINT_VALUE = args[3]
 		strtime = time.strftime(HMSFMT,time.localtime())
 		DATAPOINT_COMMENT = f"added at {strtime} by beecli"
@@ -69,11 +112,15 @@ if __name__=="__main__":
 		time.sleep(2)
 		quit()
 	try:
-		bmndr.create_datapoint(BEEMINDER_USER, BEEMINDER_GOAL, DATAPOINT_TIME, DATAPOINT_VALUE, 
+		print("creating beeminder datapoint")
+		res = bmndr.create_datapoint(BEEMINDER_USERNAME, BEEMINDER_GOAL, DATAPOINT_TIME, DATAPOINT_VALUE, 
 			comment=DATAPOINT_COMMENT)
+		print(res)
+		print("beeminder datapoint sent")
 		with open(logfile, 'a+') as f:
 			f.write(f"added datapoint of {DATAPOINT_VALUE} to {BEEMINDER_GOAL} at {strtime}\n")
 	except:
+		print("in except case")
 		with open(logfile, 'a+') as f:
 			f.write(f"failed to add datapoint of {DATAPOINT_VALUE} to {BEEMINDER_GOAL} at {strtime}\n")
 
